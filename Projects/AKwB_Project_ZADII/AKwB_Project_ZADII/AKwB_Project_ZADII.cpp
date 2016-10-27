ed2b3ab1e80c6a8d6166131df58f097f09cc639f
Kaczy6point9 <kaczytkd@gmail.com>
2016-10-27 13:13:33 +0200

    Add files via upload

--- Projects/AKwB_Project_ZADII/AKwB_Project_ZADII/AKwB_Project_ZADII.cpp
+++ Projects/AKwB_Project_ZADII/AKwB_Project_ZADII/AKwB_Project_ZADII.cpp
@@ -6,27 +6,18 @@
 #include <list>
 #include <fstream>
 #include <string>
-#define  _crt_secure_no_warnings
 
 using namespace std;
 
 class graf
 {
-
-private:
-	int n; //ilosc wierzcholkow
-	
-	
-
 public:
 	fstream plik;
 	graf()
 	{
 		int n; //ilosc wierzcholkow
-		string linia;
-		int dlugosc_lini;
-		int pom1, pom2;
-		plik.open("C:/Users/Kaczy/Documents/Visual Studio 2013/Projects/AKwB_Project_ZADII/AKwB_Project_ZADII/graf.txt");
+		int pom1, pom2; //temp zmienne pomocnicze
+		plik.open("X:/Projects/AKwB_Project_ZADII/AKwB_Project_ZADII/graf.txt");
 		if (!plik.good())
 		{
 			cout << "Blad pliku" << endl;
@@ -34,42 +25,84 @@ public:
 		else
 		{
 			plik >> n;
-			this->n = n;
-			list <int> *wierzcholki = new list <int>[n + 1]; //lista nastÄ™pnikow wierzcholka
-			while (getline(plik, linia))
-			{
-				dlugosc_lini = linia.length();
+			int **graf1 = new int *[n];  //stworzenie tablicy dynamicznej
+			for (int i = 0; i < n; i++) //bedzie ona przechowywa³a macierz sasiedztwa 
+				graf1[i] = new int[n];  //
+			graf1 = macierzsasiedztwa(n, &plik);
 
-				char *buffer_lini = new char[dlugosc_lini];
-				//strcpy(buffer_lini, linia.c_str());
-				//for (int j = 0; j < n; j++)
-				for (int i = 3; i < dlugosc_lini; i += 2)
-				{
+			plik.close();
+			list <int> *nastepniki = new list <int>[n + 1]; //lista nastepnikow kazdego wierzcholka
+			nastepniki = listasasiedztwa(graf1, n);
 
-					pom1 = atoi(&linia[0]);
-					pom2 = atoi(&linia[i]);
-					wierzcholki[pom1].push_back(pom2);
-				}
-
-			};
+			
+			//wywswietlanie listy
 			list<int>::iterator it; //iterator listy
 			for (int i = 0; i < n; i++)
 			{
-				cout << i << ": ";
-				for (it = wierzcholki[i].begin(); it != wierzcholki[i].end(); ++it)
-				{
-					cout << *it << " "; //wypisanie listy nastepnikow
-				}
-
-				cout << endl;
+			cout << i+1 << ": ";
+			for (it = nastepniki[i].begin(); it != nastepniki[i].end(); ++it)
+			{
+			cout << *it+1 << " "; //wypisanie listy nastepnikow
 			}
 
 			cout << endl;
+			}
+			
+			/*
+			//wyswietlanie macierzy
+			for (pom1 = 0; pom1 < n; pom1++) //zmienna pomocnicza, w tym przypadku bêdzie oznacza³a indeks kolumny
+			{
+			for (int pom2 = 0; pom2 < n; pom2++) //zmienna pomocnicza, w tym przypadku bêdzie oznacza³a indeks wierszu
+			{
+
+			cout << graf1[pom1][pom2] << " ";
+			}
+			cout << endl;
+			}
+			*/
+			
 		}
 
-		plik.close();
+		
+		
+
+		
 	}
-	
+	int **macierzsasiedztwa(int n, fstream *file) //gdzie n ilosc wierzcholkow
+	{
+		int pom3; //temp zmienne pomocnicze
+		int **msas = new int *[n];  //msas = macierz sasiedztwa
+		for (int i = 0; i < n; i++) // tworzenie tablicy dwuwymiarowej
+			msas[i] = new int[n];
+		
+		for (int pom1 = 0; pom1 < n; pom1++) //zmienna pomocnicza, w tym przypadku bêdzie oznacza³a indeks kolumny
+		{
+			for (int pom2 = 0; pom2 < n; pom2++) //zmienna pomocnicza, w tym przypadku bêdzie oznacza³a indeks wierszu
+			{
+				plik >> pom3;
+				msas[pom1][pom2] = pom3;
+			}
+		}
+
+		return msas; // zwracamy macierz
+	}
+	list <int> *listasasiedztwa(int **macierzsasiedztwa, int n)//tworzy liste nastepnikow z macierzy sasiedztwa
+	{
+
+		list <int> *wierzcholki = new list <int>[n + 1]; //utworzenie listy ktora bedzie przechowywa³a wierzcho³ki
+		for (int pom = 0; pom < n; pom++)
+		{
+			for (int pom2 = 0; pom2 < n; pom2++)
+			{
+				if (macierzsasiedztwa[pom][pom2] == 1)
+					wierzcholki[pom].push_back(pom2); //wstawianie na listê nastêpników danego wierzcho³ka
+			}
+		}
+
+		
+
+		return wierzcholki;
+	}
 
 };
 
@@ -77,9 +110,9 @@ public:
 
 
 int _tmain(int argc, _TCHAR* argv[])
-{ 
+{
 	graf *graf1 = new graf();
-	getchar();
+	system("PAUSE");
 	delete graf1;
 	return 0;
 }
